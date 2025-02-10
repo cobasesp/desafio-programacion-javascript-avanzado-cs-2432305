2458e797d5f5d7ca3d22a66080a82e78ce6adeae
cobasesp <cobasesp.developer@gmail.com>
2025-02-10 11:36:08 +0100

    Solucion ejercicio 04

--- 04_obra_de_arte/ejercicio/js/index.js
+++ 04_obra_de_arte/ejercicio/js/index.js
@@ -1,4 +1,23 @@
 
 // Crear una clase que cargue una imágen y que se muestre en la página.
 
-const rutaImagen = '../../imagenes/Cat_inside_LIL_134199.jpg';
+const rutaImagen = '../../imagenes/finally_over.png';
+
+class LoadImage {
+    url;
+
+    constructor(ruta){
+        this.url = ruta;
+    }
+
+    load() {
+        var newImg = document.createElement('img');
+        newImg.src = this.url;
+        document.body.appendChild(newImg);
+    }
+
+
+}
+
+const loadImage = new LoadImage(rutaImagen);
+loadImage.load();
